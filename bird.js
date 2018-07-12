3f147e0898c1c9e17eb9510bb395c2d80c087bb4
Japkeerat <japkeerat21@gmail.com>
2018-07-12 21:47:14 +0530

    Basic version of the game.

---
+++ bird.js
@@ -0,0 +1,27 @@
+function Bird(){
+    this.y = height/2;
+    this.x = 64;
+    
+    this.gravity = 0.5;
+    this.lift = 8;
+    this.velocity = 0;
+
+    this.show = function() {
+        fill(255);
+        ellipse(this.x, this.y, 32, 32);
+    };
+    
+    this.update = function() {
+        this.velocity+=this.gravity;
+        this.y+=this.velocity;
+
+        if (this.y>height || this.y<0) {
+            this.y = height;
+            this.velocity=0;
+        }
+    };
+
+    this.up = function () {
+        this.velocity-=this.lift;
+    }
+}
